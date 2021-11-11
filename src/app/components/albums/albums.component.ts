de6030b192075ec0c7ca41bd77e1fbc587ca0373
Mateo Castaño <matew-17@hotmail.com>
2021-11-10 22:07:32 -0500

    Removing non used variables

--- src/app/components/albums/albums.component.ts
+++ src/app/components/albums/albums.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { finalize } from 'rxjs/operators';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { AlbumsService } from './../../services/albums.service';
 
